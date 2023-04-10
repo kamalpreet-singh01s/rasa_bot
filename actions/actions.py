cd2404966672323bc559c23e5a755febde8b209b
chandan <chandan.gupta@01s.in>
2023-04-10 11:25:03 +0530

    web app for chatbot

--- actions/actions.py
+++ actions/actions.py
@@ -1,18 +1,18 @@
 from typing import Any, Text, Dict, List
-
 from rasa_sdk import Action, Tracker
 from rasa_sdk.executor import CollectingDispatcher
 
 
-class ActionHelloWorld(Action):
+class ActionClientQuery(Action):
 
     def name(self) -> Text:
-        return "action_hello_world"
+        return "action_client_query"
 
     def run(self, dispatcher: CollectingDispatcher,
             tracker: Tracker,
             domain: Dict[Text, Any]) -> List[Dict[Text, Any]]:
-
         dispatcher.utter_message(text="Hello World!")
+        intent = tracker.latest_message['intent'].get("name")
+        entities = tracker.latest_message['entities'].get("name")
 
         return []
