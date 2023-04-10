009868ee44eab58818c03535f41c8b143dbce40d
kamalpreet.singh <kamalpreet.singh@01s.in>
2023-04-10 18:02:50 +0530

    Code optimizations

--- app.py
+++ app.py
@@ -1,24 +1,34 @@
-from flask import Flask, redirect, url_for, request, render_template
-import requests
 import json
-from actions.actions import ActionClientQuery
 
+import requests
+from flask import Flask, request, render_template
+
+from actions.actions import ActionClientQuery
 
 app = Flask(__name__, template_folder='Templates')
 context_set: ""
 actions = ActionClientQuery()
 
 
-@app.route('/', methods = ['POST', 'GET'])
+@app.route('/', methods=['POST', 'GET'])
 def index():
+    val = ''
     if request.method == 'GET':
-        val = str(request.args.get('text'))
+        val = request.args.get('text')
+        data = json.dumps({"sender": "Rasa", "message": val})
+        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
+        res = requests.post('http://localhost:5005/webhooks/rest/webhook', data=data, headers=headers)
+        res = res.json()
+        val = res[0]['text']
+    elif request.method == 'POST':
+        val = request.form.get('text')
         data = json.dumps({"sender": "Rasa", "message": val})
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         res = requests.post('http://localhost:5005/webhooks/rest/webhook', data=data, headers=headers)
         res = res.json()
         val = res[0]['text']
     return render_template('index.html', val=val)
+
 
 if __name__ == '__main__':
     app.run(debug=True)
