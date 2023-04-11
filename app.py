1a466cf56c2c179279b7e473035fc1d1b84ef3bd
kamalpreet.singh <kamalpreet.singh@01s.in>
2023-04-11 09:52:37 +0530

    Code optimizations

--- app.py
+++ app.py
@@ -12,21 +12,15 @@ actions = ActionClientQuery()
 
 @app.route('/', methods=['POST', 'GET'])
 def index():
-    val = ''
-    if request.method == 'GET':
-        val = request.args.get('text')
-        data = json.dumps({"sender": "Rasa", "message": val})
-        headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
-        res = requests.post('http://localhost:5005/webhooks/rest/webhook', data=data, headers=headers)
-        res = res.json()
-        val = res[0]['text']
-    elif request.method == 'POST':
+    if request.method == 'POST':
         val = request.form.get('text')
         data = json.dumps({"sender": "Rasa", "message": val})
         headers = {'Content-type': 'application/json', 'Accept': 'text/plain'}
         res = requests.post('http://localhost:5005/webhooks/rest/webhook', data=data, headers=headers)
         res = res.json()
-        val = res[0]['text']
+        val = res[0].get('text')
+    else:
+        val = ''
     return render_template('index.html', val=val)
 
 
